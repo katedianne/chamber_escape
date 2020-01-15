9cd6b3cb7dcfb84a2cdde634cef3f04d3264bbbc
katedianne <hugokatedianne@gmail.com>
2020-01-15 17:57:34 +0800

    add music, change title color, add background, fix object posistions

--- app/src/main/java/com/example/atry/MainActivity.java
+++ app/src/main/java/com/example/atry/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.atry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        player = MediaPlayer.create(this, R.raw.music);
+        player.setLooping(true);
+        player.start();
+    }
+
+    public void play(View v){
+        if (player == null){
+            player = MediaPlayer.create(this, R.raw.music);
+            player.setLooping(true);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    stopPlayer();
+                }
+            });
+        }
+        player.start();
+    }
+
+    public void pause(View v){
+        if (player != null){
+            player.pause();
+        }
+    }
+
+    public void stop(View v){
+        stopPlayer();
+    }
+
+    public void stopPlayer(){
+        if (player != null){
+            player.release();
+            player = null;
+            Toast.makeText(this, "MediaPlayer released", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopPlayer();
     }
 }
